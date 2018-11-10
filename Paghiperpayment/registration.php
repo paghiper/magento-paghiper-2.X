e8c4b1ac622f44d32e9809ebc40dde91154e61fb
josesilvasilva <josesilvasilva@live.com>
2018-11-10 21:03:28 -0200

    Module PagHiper Payment Bank Slip - Magento 2

---
+++ Paghiperpayment/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PagHiper_Paghiperpayment',
+    __DIR__
+);
--- README.md
+++
@@ -1,2 +0,0 @@
-# magento-paghiper-2.X
-futuro projeto para o magento 2.X
